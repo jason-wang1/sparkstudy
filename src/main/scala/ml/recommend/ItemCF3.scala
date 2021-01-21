3944256fc6aa9fd3113123b2258c8dc0a0b9b419
wangbo <1016765655@qq.com>
2021-01-21 09:08:46 +0800

    change crossJoin to inner join

--- src/main/scala/ml/recommend/ItemCF3.scala
+++ src/main/scala/ml/recommend/ItemCF3.scala
@@ -7,7 +7,7 @@ import org.apache.spark.sql.functions._
 /**
   * Descreption:
   * 最佳方案：不采用任何数据结构(哈希表，有序数组)表示物品的评分向量，
-  * 而是直接对(userId, itemId, score)评分表进行笛卡尔积。
+  * 而是直接让(userId, itemId, score)评分表与自己join。
   * 在笛卡尔积时，如果两边表的userId匹配不上，直接就删除掉，因此笛卡尔积后的表相对来说会很小
   *
   * Date: 2020年11月23日
@@ -49,18 +49,20 @@ object ItemCF3 {
       .cache()
     itemLenDF.show(false)
 
-    val item2itemDF: DataFrame = df.alias("t1").crossJoin(df.alias("t2"))
-      .where($"t1.userId" === $"t2.userId" && $"t1.itemId" < $"t2.itemId")
-      .selectExpr("t1.itemId as itemId1", "t2.itemId as itemId2", "t1.score * t2.score as squ")
+    val item2itemDF: DataFrame = df.alias("t1")
+      .join(df.alias("t2"),
+        $"t1.userId" === $"t2.userId" && $"t1.itemId" =!= $"t2.itemId",
+        "inner")
+      .selectExpr("t1.itemId as itemId1", "t2.itemId as itemId2", "t1.score * t2.score as mul")
       .groupBy($"itemId1", $"itemId2")
-      .agg(sum($"squ").alias("squareSum"))
+      .agg(sum($"mul").alias("dot"))
       .cache()
     item2itemDF.show(false)
 
     val itemSimDF: DataFrame = item2itemDF
       .join(itemLenDF.alias("item1"), $"itemId1" === $"item1.itemId")
       .join(itemLenDF.alias("item2"), $"itemId2" === $"item2.itemId")
-      .withColumn("dot", $"squareSum" / ($"item1.vecLen" * $"item2.vecLen"))
+      .withColumn("cosSim", $"dot" / ($"item1.vecLen" * $"item2.vecLen"))
     itemSimDF.show(false)
   }
 }
