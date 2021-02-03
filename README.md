ae946f5f0a9cd6fdc237b39d6ecba6b4d59b3cfc
wangbo <1016765655@qq.com>
2021-02-03 17:46:12 +0800

    add UdfAcc

--- README.md
+++ README.md
@@ -12,6 +12,7 @@
 | ---- | ---- | ---- |
 |   [WordCount](https://github.com/jason-wang1/sparkstudy/blob/master/src/main/scala/rdd/WordCount.scala)   |   flatMap / map / reduceByKey / sortBy   |   给一个RDD[String]，进行分词，word count 并倒序排序   |
 |   [NumAcc](https://github.com/jason-wang1/sparkstudy/blob/master/src/main/scala/rdd/NumAcc.scala)   |   reduce / Spark原生累加器   |   给一个RDD[Long] / RDD[Double]，全量求和   |
+|   [UdfAcc](https://github.com/jason-wang1/sparkstudy/blob/master/src/main/scala/rdd/UdfAcc.scala)   |   自定义累加器   |   给一个RDD[Int]，全量求和   |
 
 ### 2.DataFrame
 | 编号   |  考点   |  简介   |
--- src/main/scala/rdd/UdfAcc.scala
+++ src/main/scala/rdd/UdfAcc.scala
@@ -1,16 +1,16 @@
-package com.sparkstudy.day11
+package rdd
 
 import org.apache.spark.rdd.RDD
+import org.apache.spark.util.AccumulatorV2
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
-  * Descreption: 使用自定义累加器
-  * Date: 2019年07月08日
+  * 输入：RDD[Int]
+  * 输出：全量求和
   *
-  * @author WangBo
-  * @version 1.0
+  * 使用自定义累加器对方式实现
   */
-object AccumulatorDemo3 {
+object UdfAcc {
   def main(args: Array[String]): Unit = {
     val conf: SparkConf = new SparkConf()
     conf.setAppName("accumulatorDemo").setMaster("local")
@@ -38,4 +38,31 @@ object AccumulatorDemo3 {
 
     sc.stop()
   }
+}
+
+/**
+  * 自定义Accumulator，要实现多个方法
+  */
+class MyAccumulator extends AccumulatorV2[Int, Int] {
+  private var sum = 0
+
+  override def isZero: Boolean = sum==0
+
+  override def copy(): AccumulatorV2[Int, Int] = {
+    val myacc = new MyAccumulator
+    myacc.sum = this.sum
+    myacc
+  }
+
+  override def reset(): Unit = 0
+
+  override def add(v: Int): Unit = {
+    sum+=v
+  }
+
+  override def merge(other: AccumulatorV2[Int, Int]): Unit = {
+    sum += other.value
+  }
+
+  override def value: Int = sum
 }
