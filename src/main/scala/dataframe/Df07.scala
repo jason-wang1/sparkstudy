c7ebe76e1629b8642b13e5f025beefdbc48260e0
wangbo <1016765655@qq.com>
2021-02-03 09:55:26 +0800

    completed

--- src/main/scala/dataframe/Df07.scala
+++ src/main/scala/dataframe/Df07.scala
@@ -5,9 +5,9 @@ import ulits.SparkConfig
 
 /**
   * 输入：schema为（商户id, 交易金额, 交易日期）
-  * 输出：schema为（商户id, 最近2天的交易金额, 最近3天的交易金额, 交易日期）
+  * 输出：schema为（商户id, 最近2天的交易总金额, 最近3天的交易总金额, 交易日期）
   *
-  * 输出每一天每个商户在最近2天、3天的交易金额
+  * 输出每一天每个商户在最近2天的交易总金额、最近3天的交易总金额
   */
 object Df07 {
   def main(args: Array[String]): Unit = {
@@ -20,28 +20,43 @@ object Df07 {
       ("m01", 4, "2017/1/21"),
       ("m01", 6, "2017/1/22"),
       ("m03", 1, "2017/1/23"),
-      ("m03", 5, "2017/2/23"),
+      ("m03", 5, "2017/1/23"),
       ("m02", 2, "2017/1/23"),
       ("m01", 5, "2017/1/24"),
       ("m02", 3, "2017/1/25"),
       ("m03", 2, "2017/1/25"),
       ("m01", 1, "2017/1/26"),
-      ("m04", 6, "2017/2/26")
+      ("m04", 6, "2017/1/26")
     ).toDF("mid", "amt", "date")
 
     df.createOrReplaceTempView("trans")
 
     spark.sql(
       """
-        |select *,
-        |sum(amt) over(partition by mid order by date rows between 2 preceding and current row) sum_2,
-        |sum(amt) over(partition by mid order by date rows between 3 preceding and current row) sum_3
+        |select aaa.mid, aaa.date,
+        |sum(aaa.amt) over(partition by aaa.mid order by aaa.date rows between 1 preceding and current row) sum_amt_2,
+        |sum(aaa.amt) over(partition by aaa.mid order by aaa.date rows between 2 preceding and current row) sum_amt_3
         |from (
-        |   select mid, date, sum(amt) amt
-        |   from trans
-        |   group by mid, date) aa
-        |order by date, mid
-        |""".stripMargin).show(false)
+        | select aa.mid, aa.date, sum(nvl(bb.amt, 0)) amt
+        | from (
+        |   select a.mid, b.date
+        |   from (
+        |     select distinct mid
+        |     from trans
+        |   ) a
+        |   cross join (
+        |     select distinct date
+        |     from trans
+        |   ) b
+        | ) aa
+        | left join
+        | trans bb
+        | on aa.mid = bb.mid
+        | and aa.date = bb.date
+        | group by aa.date, aa.mid
+        |) aaa
+        |order by aaa.date, aaa.mid
+        |""".stripMargin).show(100, truncate = false)
 
 
   }
